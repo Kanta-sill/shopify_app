0ae031cd4248dd3ffd5207514ff8b4ea45bc1187
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2021-06-10 15:09:55 +0700

    Update shopify_app.py
    new commit

--- models/shopify_app.py
+++ models/shopify_app.py
@@ -10,4 +10,5 @@ class ShopifyApp(models.Model):
     secret_key = fields.Char(string='Secret Key')
     api_version = fields.Char(string='API Version')
     app_name = fields.Char(string='App Name')
+    base_url = fields.Char(string='Base URL')
 
