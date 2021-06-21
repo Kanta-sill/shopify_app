58edd0977f4443c8b6a0730fe658d24bf4eb9e22
KantaSill <duc200032@gmail.com>
2021-06-21 10:43:24 +0700

    new commit

--- models/shopify_analytic.py
+++ models/shopify_analytic.py
@@ -5,5 +5,13 @@ class ShopifyAnalytic(models.Model):
     _name = "shopify.analytic"
     _description = "Shopify App"
 
-    shop_id = fields.Many2one('shopify.shop', string='Shop ID')
+    def get_discount_shop(self):
+        user_current = self.env['res.users'].sudo().search([('id', '=', self._uid)])
+        shop_current = self.env['shopify.shop'].sudo().search([('base_url', '=', user_current.login)])
+        if shop_current:
+            return shop_current.id
+        else:
+            return None
+
+    shop_id = fields.Many2one('shopify.shop', string='Shop ID', default=get_discount_shop)
     name = fields.Char(string='Name')
